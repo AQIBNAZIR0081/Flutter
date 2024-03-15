b521ed8f87edfd52afdebddc2686bb365fbc5812
AQIBNAZIR0081 <aqibnazir0081@gmail.com>
2024-03-15 22:47:40 +0500

    providing dummy questions data

---
+++ quiz_app/lib/data/questions_data.dart
@@ -0,0 +1,94 @@
+import '../models/quiz_questions.dart';
+
+var questions = [
+  QuizQuestions(
+    'Which IDE (Integrated Development Environment) is commonly used for Flutter development?',
+    [
+      'Android Studio',
+      'Visual Studio Code',
+      'Xcode',
+      'Eclipse',
+    ],
+  ),
+  QuizQuestions(
+    'What is the Flutter framework primarily used for?',
+    [
+      'Web development',
+      'Mobile app development',
+      'Game development',
+      'Desktop application development',
+    ],
+  ),
+  QuizQuestions(
+    'Which widget in Flutter is used for creating a scrollable list of children?',
+    [
+      'ListView',
+      'GridView',
+      'SingleChildScrollView',
+      'Column',
+    ],
+  ),
+  QuizQuestions(
+    'What command is used to create a new Flutter project from the command line interface (CLI)?',
+    [
+      'flutter init',
+      'flutter new',
+      'flutter create',
+      'flutter start',
+    ],
+  ),
+  QuizQuestions(
+    'Which package management tool is used in Flutter for adding external dependencies to a project?',
+    [
+      'npm',
+      'Gradle',
+      'CocoaPods',
+      'pub',
+    ],
+  ),
+  QuizQuestions(
+    'What is the primary function of the setState() method in Flutter?',
+    [
+      'To update the state of a widget',
+      'To define the layout of a widget',
+      'To navigate to a different screen',
+      'To fetch data from an API',
+    ],
+  ),
+  QuizQuestions(
+    'What file is used to declare the applications UI structure in Flutter?',
+    [
+      'main.dart',
+      'app.js',
+      'index.html',
+      'styles.css',
+    ],
+  ),
+  QuizQuestions(
+    'Which widget in Flutter is used for displaying images?',
+    [
+      'ImageBox',
+      'ImageView',
+      'Image',
+      'Picture',
+    ],
+  ),
+  QuizQuestions(
+    'What is the purpose of a MaterialApp widget in a Flutter application?',
+    [
+      'To define the applications entry point',
+      'To configure the overall theme and navigation',
+      'To handle HTTP requests',
+      'To manage device sensors',
+    ],
+  ),
+  QuizQuestions(
+    'Which command is used to run a Flutter app in debug mode on an Android device or emulator?',
+    [
+      'flutter deploy',
+      'flutter build',
+      'flutter run',
+      'flutter launch',
+    ],
+  ),
+];
