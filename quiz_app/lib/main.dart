f2a3ebe205de8df23341659aa08524559ccc0ab2
AQIBNAZIR0081 <aqibnazir0081@gmail.com>
2024-03-20 22:50:23 +0500

    changes in files

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,14 +1,7 @@
-// Import the generated file
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:quiz_app/quiz.dart';
 
-Future<void> main() async {
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
+void main() {
   runApp(
     const Quiz(),
   );
