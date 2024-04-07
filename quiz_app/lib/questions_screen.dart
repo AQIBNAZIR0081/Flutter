78a4f7edd7a07c70476bd96336cad4112def927a
AQIBNAZIR0081 <aqibnazir0081@gmail.com>
2024-04-08 01:34:21 +0500

    Mutating values in Memory

--- quiz_app/lib/questions_screen.dart
+++ quiz_app/lib/questions_screen.dart
@@ -15,24 +15,29 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
     final currentQuestion = questions[0];
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            currentQuestion.text,
-            style: const TextStyle(color: Colors.white),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
 
-          // Spreading  the options into a list so that we can use it in the widget AnswerButton
-          ...currentQuestion.answers.map(
-            (answer) {
-              return AnswerButton(answerText: answer, onTap: () {});
-            },
-          ),
-        ],
+            // Spreading  the options into a list so that we can use it in the widget AnswerButton
+            ...currentQuestion.getShuffledAnswers().map(
+              (answer) {
+                return AnswerButton(answerText: answer, onTap: () {});
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
