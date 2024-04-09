e1b7677d21e83f60076d12655bba24bb27fe8a10
AQIBNAZIR0081 <aqibnazir0081@gmail.com>
2024-04-09 16:15:24 +0500

    Filtering and Analyzing list

--- quiz_app/lib/questions_summary.dart
+++ quiz_app/lib/questions_summary.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class QuestoinsSummary extends StatelessWidget {
   const QuestoinsSummary(this.summaryData, {super.key});
@@ -12,7 +13,17 @@ class QuestoinsSummary extends StatelessWidget {
         (data) {
           return Row(
             children: [
-              Text(((data['question'] as int) + 1).toString()),
+              Text(((data['question_index'] as int) + 1).toString()),
+              Expanded(
+                child: Column(
+                  children: [
+                    Text(data['question'].toString()),
+                    const SizedBox(height: 5),
+                    Text(data['user_answer'].toString()),
+                    Text(data['correct_answer'].toString()),
+                  ],
+                ),
+              ),
             ],
           );
         },
