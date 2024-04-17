ab623a4fe5676b936d03da2140d010e4aa2530a4
AQIB NAZIR <144234362+AQIBNAZIR0081@users.noreply.github.com>
2024-04-17 22:17:34 +0500

    Update main.dart

--- expense_tracker_app/lib/main.dart
+++ expense_tracker_app/lib/main.dart
@@ -1,10 +1,1 @@
-import 'package:expense_tracker_app/expenses.dart';
-import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    const MaterialApp(
-      home: Expenses(),
-    ),
-  );
-}
