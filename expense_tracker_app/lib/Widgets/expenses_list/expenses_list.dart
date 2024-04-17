815d6479464cf5c9d40a955569812a0a51d39d4b
AQIBNAZIR0081 <aqibnazir0081@gmail.com>
2024-04-17 22:10:48 +0500

    Create a Custome List Item with the Card

---
+++ expense_tracker_app/lib/Widgets/expenses_list/expenses_list.dart
@@ -0,0 +1,20 @@
+import 'package:expense_tracker_app/Widgets/expenses_list/expense_item.dart';
+import 'package:expense_tracker_app/model/expense.dart';
+import 'package:flutter/material.dart';
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList({
+    super.key,
+    required this.expenses,
+  });
+
+  final List<Expense> expenses;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (context, index) => ExpenseItem(expenses[index]),
+    );
+  }
+}
--- expense_tracker_app/lib/main.dart
+++ expense_tracker_app/lib/main.dart
@@ -1,5 +1,10 @@
+import 'package:expense_tracker_app/Widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp());
+  runApp(const MaterialApp(
+    home: Scaffold(
+      body: Expenses(),
+    ),
+  ));
 }
