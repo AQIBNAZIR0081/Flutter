6954acefed36b1a85e8bf41fe98bdb9055b756e4
AQIBNAZIR0081 <aqibnazir0081@gmail.com>
2024-04-22 01:04:56 +0500

    Updating Color Scheme of App

--- expense_tracker_app/lib/main.dart
+++ expense_tracker_app/lib/main.dart
@@ -2,13 +2,18 @@ import 'package:expense_tracker_app/Widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  var kColorScheme = ColorScheme.fromSeed(
+    seedColor: const Color.fromARGB(255, 96, 59, 181),
+  );
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData().copyWith(
         scaffoldBackgroundColor: const Color.fromARGB(255, 24, 186, 192),
-        appBarTheme: const AppBarTheme(
-            backgroundColor: Color.fromARGB(118, 34, 92, 252)),
+        colorScheme: kColorScheme,
+        appBarTheme: AppBarTheme(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.primaryContainer),
       ),
       home: const Expenses(),
     ),
